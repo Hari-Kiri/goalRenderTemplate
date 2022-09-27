5c02baf80dd632116f826ab50092f8b9984e93e8
@Hari-Kiri <pratamahari89@gmail.com>
2022-09-28 00:31:55 +0700

    Use temboLog for logging.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Hari-Kiri/goalRenderTemplate
 
 go 1.18
 
-require github.com/Hari-Kiri/goalApplicationSettingsLoader v0.1.0
+require (
+	github.com/Hari-Kiri/goalApplicationSettingsLoader v0.1.0
+	github.com/Hari-Kiri/temboLog v0.1.3
+)
--- goalRenderTemplate.go
+++ goalRenderTemplate.go
@@ -2,21 +2,22 @@ package goalRenderTemplate
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 
 	"github.com/Hari-Kiri/goalApplicationSettingsLoader"
+	"github.com/Hari-Kiri/temboLog"
 )
 
 // HTML renderer
-func Process(htmlTemplates *template.Template, responseWriter http.ResponseWriter, template string, appSettings *goalApplicationSettingsLoader.ApplicationSettings, request *http.Request) {
+func Process(htmlTemplates *template.Template, responseWriter http.ResponseWriter, template string,
+	appSettings *goalApplicationSettingsLoader.ApplicationSettings, request *http.Request) {
 	// Open HTML page
 	error := htmlTemplates.ExecuteTemplate(responseWriter, template+".html", appSettings)
-	log.Println("[info] Serving " + template + " page, requested from " + request.RemoteAddr + " with request url path [" + request.URL.Path + "]")
+	temboLog.InfoLogging("Serving", template, "page, requested from", request.RemoteAddr, "with request url path [",
+		request.URL.Path, "]")
 	// Failed when opening HTML page
 	if error != nil {
 		// Give 500 response code
 		http.Error(responseWriter, error.Error(), http.StatusInternalServerError)
 	}
 }
-
